5be9d604b04aa9b796816ce321bfcdfa3950bd7a
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-05-13 23:38:46 +0900

    Typescript

---
+++ dist/Hello.js
@@ -0,0 +1,8 @@
+"use strict";
+const messege = "Hello World";
+console.log(messege);
+for (let i = 0; i < 5; i++) {
+    console.log(i + "Hello Typescript");
+}
+console.log("Hello Typescript");
+//# sourceMappingURL=Hello.js.map
---
+++ src/Hello.ts
@@ -0,0 +1,6 @@
+const messege : String = "Hello World";
+console.log(messege);
+for (let i = 0; i < 5; i++) {
+    console.log(i + "Hello Typescript");
+}
+console.log("Hello Typescript");
